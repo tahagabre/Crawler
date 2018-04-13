be0b476123553f08af85f41ddf71cba9ad0e1bfb
Taha Gabre <tgabre@honeygrow.com>
2018-04-13 16:10:56 -0400

    Player moves, never stops

--- Crawler Game/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('');
-    console.log("listening on 8080");
-}).listen(8080);
